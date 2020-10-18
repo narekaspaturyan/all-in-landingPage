942a5bb7a76bbe15f48321edd555119654add250
narekaspaturyan <narek.aspaturyan@gmail.com>
2020-10-18 18:56:14 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 750 ||
+    document.documentElement.scrollTop > 750
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  //   pure js
+  //   document.body.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  //   document.documentElement.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  $("html,body").animate({ scrollTop: 0 }, "slow"); //fast jQuery
+}
+
+//               ----scroll using jquery----
+// $(function () {
+//   $(".nav a").on("click", function () {
+//     $("html, body").animate(
+//       { scrollTop: $($.attr(this, "href")).offset().top },
+//       500
+//     );
+//   });
+// });
